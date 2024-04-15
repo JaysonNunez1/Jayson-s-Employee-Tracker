0537401326dbb96aa8fcda7efb4485a2e9ddfaae
JaysonNunez1 <jaysonnunez3@gmail.com>
2024-04-14 22:09:53 -0400

    created mysql database connection and created a function to start the tracker once it makes a connection

--- server.js
+++ server.js
@@ -10,3 +10,37 @@ const connection = mysql.createConnection ({
     password: process.env.DB_PASS,
     database: process.env.DB_DBASE
 });
+
+connection.connect(function (err){
+    if (err) throw err;
+    startTracker();
+});
+
+function startTracker(){
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'UserChoice',
+            message:'What would you like to do?',
+            choices: [
+             'View all Departments',
+             'View all Roles',
+             'View all Employees',
+             'Add a Department',
+             'Add a Role',
+             'Add an Employee',
+             'Update an Employee Role',
+             'Delete an Employee',
+             'Delete a Role',
+             'Delete a Department',
+             'Exit'
+            ]
+        }
+             
+
+
+
+             '
+
+    ])
+}
