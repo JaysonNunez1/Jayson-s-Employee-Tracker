10901fb9b55a92a48560bb9eae2f2bccd44a3aff
JaysonNunez1 <jaysonnunez3@gmail.com>
2024-04-17 21:37:59 -0400

    created a function to add departments

--- server.js
+++ server.js
@@ -114,3 +114,33 @@ function viewAllRoles() {
         startTracker();
     });
 };
+
+function viewAllEmployees() {
+    let query = 'SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager '
+        'FROM employee '
+        'LEFT JOIN role ON employee.role_id = role.id '
+        'LEFT JOIN department ON department_id = role.department_id '
+        'LEFT JOIN employee manager ON manager.id = employee.manager_id';
+    connection.query(query, function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        startTracker();
+    });
+};
+
+function addDepartment() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'Name',
+            message:'Department name:'
+        }
+        ]).then((res)=>{
+            let query = 'INSERT INTO department SET ?'
+            connection.query(query, {name: res.Name},(err,res) => {
+                if(err) throw err;
+                console.log('Department added!');
+                startTracker();
+        });
+    });
+};
